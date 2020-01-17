e58d17ee1b1f7b2746e5f800a68473d694748d89
Codetoil <ianthisawesomee@gmail.com>
2020-01-16 21:09:16 -0500

    update long

--- src/main/java/io/github/codetoil/autoafkfisher/AutoAfkFisher.java
+++ src/main/java/io/github/codetoil/autoafkfisher/AutoAfkFisher.java
@@ -6,7 +6,7 @@ public class AutoAfkFisher implements ModInitializer
 {
 	public static boolean isafkon = false;
 	public static int lastPressedF12 = 0;
-	public static final String VERSION = "{mod=(1.0.0.1), mappings=(1.14.4+build.9), loader=(0.4.8+build.159), fabric=(0.3.1+build.208)}";
+	public static final String VERSION = "{mod=(1.0.0.1), mappings=(1.14.4+build.15), loader=(0.7.4+build.177), fabric=(0.4.2+build.246), mc=(1.14.4)}";
 
 	@Override
 	public void onInitialize()
