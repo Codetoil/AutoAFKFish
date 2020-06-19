11f43c0d46b60f6e1aaba5859b18a960732917fe
Codetoil <ianthisawesomee@gmail.com>
2020-06-18 23:32:15 -0400

    update to 1.16-rc1

--- src/main/java/io/github/codetoil/autoafkfisher/mixin/InputUtilMixin.java
+++
@@ -1,9 +0,0 @@
-package io.github.codetoil.autoafkfisher.mixin;
-
-import net.minecraft.client.util.InputUtil;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(InputUtil.class)
-public abstract class InputUtilMixin
-{
-}
